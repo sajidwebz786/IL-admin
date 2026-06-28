0c83698abe6cd87f6a341f275fa43dfb95864245
sajidwebz786 <sajidwebz@gmail.com>
2026-06-29 03:39:19 +0530

    new changes from kilo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react({ fastRefresh: false })],
+  build: {
+    sourcemap: false
+  },
   server: {
     host: '0.0.0.0',
     port: 5173,
